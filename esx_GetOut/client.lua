397ec9a2d647d56e6654f97295f05f2f7ece31f3
BattleRatttt <39086978+BattleRattt@users.noreply.github.com>
2019-06-29 23:18:50 -0500

    Attempt To Fix High Client Usage + New PlayerPedId() Usage
    Shouldn't necessarily need to be called every frame. Also moved to the new PlayerPedId() call.

--- esx_GetOut/client.lua
+++ esx_GetOut/client.lua
@@ -18,16 +18,15 @@ end)
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(0)
-        	local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
-		local ped = GetPlayerPed(-1)
+		Citizen.Wait(1000)
+        	local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
 		local vehicleClass = GetVehicleClass(vehicle)
 		PlayerData = ESX.GetPlayerData()
 		
-		if vehicleClass == 18 and GetPedInVehicleSeat(vehicle, -1) == ped then
+		if vehicleClass == 18 and GetPedInVehicleSeat(vehicle, -1) == PlayerPedId() then
 			if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mecano' then
-			ClearPedTasksImmediately(ped)
-			TaskLeaveVehicle(ped,vehicle,0)
+			ClearPedTasksImmediately(PlayerPedId())
+			TaskLeaveVehicle(PlayerPedId(),vehicle,0)
 			ESX.ShowNotification("No stealing Emergency Vehicles. You have "..waitTimeInSeconds.." seconds to get out")
 			Citizen.Wait(waitTime)
 				if IsPedInVehicle(PlayerPedId(), vehicle, false)
